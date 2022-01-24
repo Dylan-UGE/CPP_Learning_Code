ad31fa93e930d16a7408935378b0e92695c7fbbe
Dylan-UGE <didicanet@aol.com>
2022-01-24 16:16:03 +0100

    Complete chap-03

--- chap-03/3-allocation/Cat.cpp
+++ chap-03/3-allocation/Cat.cpp
@@ -1,4 +1,21 @@
-int main()
-{
+#include "Cat.h"
+
+int main() {
+    std::unique_ptr<Cat> cat = std::make_unique<Cat>("Louis");
+    std::cout << *cat << std::endl;
+
+    std::unique_ptr<Cat> empty_cat;
+    if (empty_cat == nullptr) {
+        std::cout << "Null pointer" << std::endl;
+    }
+
+    empty_cat = std::make_unique<Cat>("Pixel");
+
+    std::cout << "Same pointer ? " << ((cat == empty_cat) ? "True" : "False") << std::endl;
+    std::cout << "Same name ? " << ((*cat == *empty_cat) ? "True" : "False") << std::endl;
+
+    cat.reset();
+    std::cout << "cat == nullptr ? " << ((cat == nullptr) ? "True" : "False") << std::endl;
+
     return 0;
 }
--- chap-03/4-move/Move.cpp
+++ chap-03/4-move/Move.cpp
@@ -1,4 +1,23 @@
-int main()
-{
+#include <iostream>
+#include <memory>
+
+void display(const std::string& variable_name, const std::unique_ptr<int>& variable) {
+    std::cout << variable_name << " : " << *variable << std::endl;
+}
+
+std::unique_ptr<int> passthrough(std::unique_ptr<int> i) {
+    auto tmp = std::move(i);
+
+    return tmp;
+}
+
+int main() {
+    std::unique_ptr<int> i1 = std::make_unique<int>(5);
+    std::unique_ptr<int> i2 = std::make_unique<int>(10);
+
+    display("i1", i1);
+    i2 = passthrough(std::move(i1));
+    display("i1", i1);
+
     return 0;
 }
